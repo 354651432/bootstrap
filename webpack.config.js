dd1b390392a32a75b77e8a9ac894e4188b03c4d0
dual <daul@dr.h>
2019-07-18 17:52:25 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        hmr: true,
+                        reloadAll: true,
+                    }
+                },
+                {
+                    loader: 'css-loader',
+                    options: {
+                        importLoaders: 1
+                    }
+                },
+                    'less-loader'
+                ]
+            }, {
+                test: /\.(svg|woff2|woff|ttf|eot)$/,
+                use: "file-loader"
+            }
+        ]
+    }, plugins: [
+        new HtmlWebpackPlugin({ title: "bs", template: "./src/index.html" }),
+        new MiniCssExtractPlugin({ filename: "bootstrap.css" }),
+    ]
+};
